8ef42974d12eba3fe54663d8f752ba20f0362f5a
shangnan <shangnan@bytedance.com>
2020-12-19 00:10:16 +0800

    feat: add farrow benchmark

---
+++ benchmarks/farrow.js
@@ -0,0 +1,11 @@
+import { Http, Response } from 'farrow-http'
+
+const http = Http()
+
+// add http middleware
+http.use(() => {
+  // returning response in middleware
+  return Response.text(`Hello Farrow`)
+})
+
+http.listen(3000)
